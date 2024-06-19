c009e004becbe77894a68f534278fb56612011e2
Emiliano Aguilar <emilianoa.aguilar17@gmail.com>
2024-06-19 10:10:48 -0600

    Avance con lo que dijo Deltoro

--- ArduinoFinal/ArduinoFinal.ino
+++ ArduinoFinal/ArduinoFinal.ino
@@ -10,7 +10,13 @@ const int ldrPin = A0; // Pin donde está conectado el LDR
 inconcluso hasta que se haga la tira led
 Se podría hacer con el servidor http en la rasp, que detecte el cambio y mande la peticion para prenderlo
 */
-int dPIR=2;
+const int dPIR=2;
+int estadoPIR = 0;
+
+
+const int mic = A1;
+
+const int gas = A2;
 
 void setup() {
   dht.begin();
@@ -19,24 +25,50 @@ void setup() {
 }
 
 void loop() {
-  String nivel = checkValueLDR(analogRead(ldrPin));
 
-  int ESTADO = digitalRead(dPIR);
-
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-
-  Serial.print("HM : "); Serial.print(h); Serial.print(" : % "); Serial.println(": Humedad");
-  Serial.print("TP : "); Serial.print(t); Serial.print(" : *C "); Serial.println(": Temperatura");
-  //tengo duda si mandar el caracter string o el valor tal cual ↓
-  Serial.print("FR : "); Serial.print(nivel); Serial.print(" : lux "); Serial.println(": Luz");
-  //habrá pedo en iOS por estos: ^ ↓
-  Serial.print("PIR : "); Serial.print(ESTADO); Serial.print(" : "); Serial.println(": Movimiento");
+  int lectura = Serial.read();
+  getData(lectura);
   delay(2000);
 }
 
-String checkValueLDR(int valorLDR){
-  String nivel = " ";
+void getData(int sensor) {
+  switch(sensor){
+    case 1:
+      sensorDHT11();
+      break;
+    case 2:
+      sensorLDR();
+      break;
+    case 3:
+      sensorPIR();
+      break;
+    case 4:
+      sensorSonido();
+      break;
+    case 5:
+      sensorMQ2();
+      break;
+    case 6:
+    //Peso
+      break;
+    case 7:
+    //frecuencia
+      break; 
+    case 8:
+      sensorDHT11();
+      sensorLDR();
+      sensorPIR();
+      sensorSonido();
+      sensorMQ2();
+      break; 
+    default:
+      break;
+  }
+}
+
+void sensorLDR() {
+  int valorLDR = analogRead(ldrPin);
+  String nivel = "";
   if(valorLDR >= 0 && valorLDR < 150)
   {
     nivel ="Muy alta";
@@ -57,5 +89,34 @@ String checkValueLDR(int valorLDR){
   {
     nivel = "Luz apagada";
   }
-  return nivel;
+  //tengo duda si mandar el caracter string o el valor tal cual ↓
+  Serial.print("FR : "); Serial.print(nivel); Serial.print(" : lux "); Serial.println(": Luz");
+}
+
+void sensorDHT11() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  Serial.print("HM : "); Serial.print(h); Serial.print(" : % "); Serial.println(": Humedad");
+  Serial.print("TP : "); Serial.print(t); Serial.print(" : *C "); Serial.println(": Temperatura");
+}
+
+void sensorPIR() {
+  int ESTADO = digitalRead(dPIR);
+  //habrá pedo en iOS por esto: ↓
+  Serial.print("PIR : "); Serial.print(ESTADO); Serial.print(" : "); Serial.println(": Movimiento");
+}
+
+void leerSensor(int pin, int umbral, const String& etiqueta, const String& unidad) {
+  int valor = analogRead(pin);
+  String res = (valor < umbral) ? "Bajo" : "Alto";
+  Serial.print(etiqueta); Serial.print(" : "); Serial.print(res); Serial.print(" : "); Serial.println(unidad);
+}
+
+void sensorSonido() {
+  leerSensor(mic, 105, "SD", ": Sonido");
+  delay(250);
+}
+
+void sensorMQ2() {
+  leerSensor(gas, 210, "MQ", "ppm : Gas");
 }
